dfa660a7ec61bc1c589ed4b5a8e09128d04dabb9
cristiano-97 <christhiangutierrezrosas@gmail.com>
2022-07-04 22:44:43 -0500

    adicion de mas cambios

--- src/main/java/edu/pe/idat/app/models/dao/ICliente.java
+++
@@ -1,8 +0,0 @@
-package edu.pe.idat.app.models.dao;
-
-import java.util.List;
-import edu.pe.idat.app.models.entities.Cliente;
-
-public interface ICliente {
-	public List<Cliente> listarTodo();
-}
--- src/main/java/edu/pe/idat/app/models/dao/IWeb.java
+++
@@ -1,9 +0,0 @@
-package edu.pe.idat.app.models.dao;
-
-import java.util.List;
-
-import com.app.models.entities.Web;
-
-public interface IWeb {
-	public List<Web> mostrarTodo();
-}

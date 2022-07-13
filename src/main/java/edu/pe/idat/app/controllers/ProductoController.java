12cbb399f6b2a3807bcb4f06f87f35b58d9aba97
cristiano-97 <christhiangutierrezrosas@gmail.com>
2022-07-13 12:55:03 -0500

    video 16 terminado

--- src/main/java/edu/pe/idat/app/controllers/ProductoController.java
+++ src/main/java/edu/pe/idat/app/controllers/ProductoController.java
@@ -1,5 +1,6 @@
 package edu.pe.idat.app.controllers;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import org.slf4j.*; //importa todo lo que tiene el paquete de Logger
@@ -10,10 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import edu.pe.idat.app.models.entities.Producto;
 import edu.pe.idat.app.models.entities.Usuario;
 import edu.pe.idat.app.services.ProductoService;
+import edu.pe.idat.app.services.UploadFileService;
 
 
 @Controller
@@ -25,6 +29,9 @@ public class ProductoController {
 	@Autowired //crea objetos el mismo spring
 	private ProductoService productoService; //objeto productoService
 	
+	@Autowired
+	private UploadFileService upload;
+	
 	@GetMapping("")
 	public String show(Model model) { //parámetro de tipo model,lleva información desde backend hacia la vista Show
 		model.addAttribute("productos", productoService.findAll()); // se usa el objeto model, y se invoca addAtribute con 2 parámetros, objeto productoService en donde están todos los métodos.
@@ -34,11 +41,33 @@ public class ProductoController {
 	public String create() {
 		return "productos/create";
 	}
+	
 	@PostMapping("/save")						 // responde a una peticion tipo postmapping, mapeado con /save
-	public String save(Producto producto) {			//importamos producto y --> //recibe parámetro en el objeto producto
+	public String save(Producto producto,@RequestParam("img") MultipartFile file ) throws IOException {			//importamos producto y --> //recibe parámetro en el objeto producto
 		LOGGER.info("Este es el objeto producto {}", producto);
 		Usuario u = new Usuario(1,"","","","","","",""); //crear usuario en la base de datos
 		producto.setUsuario(u);						//se añade el usuario a producto 
+		
+		//imagen
+		if(producto.getId()==null) {
+			String nombreImagen=upload.saveImage(file);
+			producto.setImagen(nombreImagen);
+			
+		}
+		
+		else {
+			//al editar el producto sin affectar la imagen 
+			//de no ser deseado
+			if(file.isEmpty()) {
+				Producto p=new Producto();
+				p=productoService.get(producto.getId()).get();
+				producto.setImagen(p.getImagen());
+			}
+			else {
+				String nombreImagen=upload.saveImage(file);
+				producto.setImagen(nombreImagen);
+			}
+		}
 		
 		productoService.save(producto);
 		return "redirect:/productos";

3ea02032f0ea326948fb11cf212cd2de0480fa60
cristiano-97 <christhiangutierrezrosas@gmail.com>
2022-07-14 02:41:12 -0500

    video 22 terminado

---
+++ src/main/java/edu/pe/idat/app/controllers/HomeController.java
@@ -0,0 +1,42 @@
+package edu.pe.idat.app.controllers;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import edu.pe.idat.app.models.entities.Producto;
+import edu.pe.idat.app.services.ProductoService;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	private final Logger log=LoggerFactory.getLogger(HomeController.class);
+	
+	@Autowired
+	private ProductoService productoService;
+	
+	@GetMapping("")
+	public String home(Model model) {
+		model.addAttribute("productos",productoService.findAll());
+		return"usuario/home";
+	}
+	
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id,Model model) {
+		log.info("id producto enviado como parametro {}",id);
+		Producto producto =new Producto();
+		Optional<Producto> productoOptional = productoService.get(id);
+		producto = productoOptional.get();
+		model.addAttribute("producto",producto);
+		return "usuario/productohome";
+	}
+
+}

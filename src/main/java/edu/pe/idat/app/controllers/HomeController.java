e7ed60ce058e6b7552172c5ec701593e5a939cba
cristiano-97 <christhiangutierrezrosas@gmail.com>
2022-07-17 23:49:10 -0500

    video 26 terminado

--- src/main/java/edu/pe/idat/app/controllers/HomeController.java
+++ src/main/java/edu/pe/idat/app/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package edu.pe.idat.app.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -9,8 +11,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import edu.pe.idat.app.models.entities.DetalleOrden;
+import edu.pe.idat.app.models.entities.Orden;
 import edu.pe.idat.app.models.entities.Producto;
 import edu.pe.idat.app.services.ProductoService;
 
@@ -22,6 +28,13 @@ public class HomeController {
 	
 	@Autowired
 	private ProductoService productoService;
+	
+	//para almacenar detalles de la orden
+	List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
+	
+	//datos de la orden 
+	Orden orden = new Orden();
+	
 	
 	@GetMapping("")
 	public String home(Model model) {
@@ -37,6 +50,34 @@ public class HomeController {
 		producto = productoOptional.get();
 		model.addAttribute("producto",producto);
 		return "usuario/productohome";
+	}
+	
+	@PostMapping("/cart")
+	public String addCart(@RequestParam Integer id,@RequestParam Integer cantidad,Model model) {
+		DetalleOrden detalleOrden = new DetalleOrden();
+		Producto producto = new Producto();
+		double sumaTotal=0;
+		
+		Optional<Producto> optionalProducto = productoService.get(id);
+		log.info("producto añadido : {}",optionalProducto.get());
+		producto=optionalProducto.get();
+		detalleOrden.setCantidad(cantidad);
+		detalleOrden.setPrecio(producto.getPrecio());
+		detalleOrden.setNombre(producto.getNombre());
+		detalleOrden.setTotal(producto.getPrecio()*cantidad);
+		detalleOrden.setProducto(producto);
+		
+		detalles.add(detalleOrden);
+		
+		
+		
+		sumaTotal=detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
+		
+		orden.setTotal(sumaTotal);
+		
+		model.addAttribute("cart",detalles);
+		model.addAttribute("orden",orden);
+		return "usuario/carrito";
 	}
 
 }
